8669b16ee51fd30143464081ccea1fd8e83f25f7
Rafael <rafaelguanciale@hotmail.com>
2026-05-07 18:00:03 -0300

    Feat: Collection page created

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [isLogged, setIsLogged] = useState(false);
+  const [isLogged, setIsLogged] = useState(true);
   const [isApiAvailable, setIsApiAvailable] = useState(false);
   const [user, setUser] = useState("");
 
