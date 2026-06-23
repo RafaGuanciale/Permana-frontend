3cd3d653254d40a968f27fefb1b70ea4740ab7eb
Rafael <rafaelguanciale@hotmail.com>
2026-06-23 16:19:27 -0300

    Refactor: Header atualizado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,51 +1,41 @@
-import loginIcon from "../../images/icons/circle-user-champagne.png";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useContext, useState } from "react";
 import { PopupContext } from "../../contexts/PopupContext";
 import { UserContext } from "../../contexts/UserContext";
 import { AuthContext } from "../../contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
 
 function Header() {
   const { user } = useContext(UserContext);
   const { isLogged } = useContext(AuthContext);
-  const { handleOpenPopup } =
-    useContext(PopupContext);
-  const navigate = useNavigate();
-
+  const { handleOpenPopup } = useContext(PopupContext);
   const handleChangePopup = () => {
-    isLogged ? handleOpenPopup("profile") : navigate("/login");
+    handleOpenPopup("profile");
   };
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  if (!isLogged) return null; 
   return (
     <header className="header" id="header">
       <div className="header__container">
         <h2 className="header__title">Aromatik</h2>
         <div className="header__navigation">
-          {isLogged && (
-            <nav className="header__nav" id="header__nav-user">
-              <NavLink to="/dashboard" className="header__nav-link">
-                Dashboard
-              </NavLink>
-              <NavLink to="/collection" className="header__nav-link">
-                Coleção
-              </NavLink>
-              <NavLink to="/analysis" className="header__nav-link">
-                Análise
-              </NavLink>
-              <NavLink to="/recommendation" className="header__nav-link">
-                Recomendações
-              </NavLink>
-              <NavLink to="/categories" className="header__nav-link">
-                Categorias
-              </NavLink>
-              {/* <NavLink to="/blog" className="header__nav-link">
-                Blog
-              </NavLink> */}
-            </nav>
-          )}
+          <nav className="header__nav" id="header__nav-user">
+            <NavLink to="/dashboard" className="header__nav-link">
+              Dashboard
+            </NavLink>
+            <NavLink to="/collection" className="header__nav-link">
+              Coleção
+            </NavLink>
+            <NavLink to="/analysis" className="header__nav-link">
+              Análise
+            </NavLink>
+            <NavLink to="/recommendation" className="header__nav-link">
+              Recomendações
+            </NavLink>
+            <NavLink to="/categories" className="header__nav-link">
+              Categorias
+            </NavLink>
+          </nav>
         </div>
         <div className="header__actions">
           <button
@@ -61,26 +51,13 @@ function Header() {
             id="header-user-btn"
             onClick={handleChangePopup}
           >
-            {isLogged ? (
-              <img
-                id="profile-icon"
-                className="header__action-icon"
-                src={user?.avatar}
-                alt="Profile"
-              />
-            ) : (
-              <img
-                id="profile-icon"
-                className="header__action-icon"
-                src={loginIcon}
-                alt="Profile"
-              />
-            )}
-            {isLogged ? (
-              <span className="header__action-text">Perfil</span>
-            ) : (
-              <span className="header__action-text">Entrar</span>
-            )}
+            <img
+              id="profile-icon"
+              className="header__action-icon"
+              src={user?.avatar}
+              alt="Profile"
+            />
+            <span className="header__action-text">Perfil</span>
           </button>
         </div>
       </div>
@@ -89,75 +66,47 @@ function Header() {
           isMenuOpen ? "header__mobile-menu--open" : ""
         }`}
       >
-        {isLogged ? (
-          <nav className="header__mobile-nav">
-            <NavLink
-              to="/dashboard"
-              className="header__mobile-link"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              Dashboard
-            </NavLink>
+        <nav className="header__mobile-nav">
+          <NavLink
+            to="/dashboard"
+            className="header__mobile-link"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            Dashboard
+          </NavLink>
 
-            <NavLink
-              to="/collection"
-              className="header__mobile-link"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              Coleção
-            </NavLink>
+          <NavLink
+            to="/collection"
+            className="header__mobile-link"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            Coleção
+          </NavLink>
 
-            <NavLink
-              to="/analysis"
-              className="header__mobile-link"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              Análise
-            </NavLink>
+          <NavLink
+            to="/analysis"
+            className="header__mobile-link"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            Análise
+          </NavLink>
 
-            <NavLink
-              to="/recommendation"
-              className="header__mobile-link"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              Recomendações
-            </NavLink>
+          <NavLink
+            to="/recommendation"
+            className="header__mobile-link"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            Recomendações
+          </NavLink>
 
-            <NavLink
-              to="/categories"
-              className="header__mobile-link"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              Categorias
-            </NavLink>
-          </nav>
-        ) : (
-          <nav className="header__mobile-nav">
-            <Link
-              to="/"
-              className="header__mobile-link"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              Home
-            </Link>
-
-            <a
-              href="#about-aromatik"
-              className="header__mobile-link"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              Produto
-            </a>
-
-            <a
-              href="#author"
-              className="header__mobile-link"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              Desenvolvedor
-            </a>
-          </nav>
-        )}
+          <NavLink
+            to="/categories"
+            className="header__mobile-link"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            Categorias
+          </NavLink>
+        </nav>
       </div>
     </header>
   );
